839a8e22e7ebe0715bf8d58a852f7dcfef49ee89
kyekang <kyekang@ebay.com>
2016-11-08 15:06:44 +0900

    Pizza test code

---
+++ src/test/scala/com/ebaykorea/kyekang/PizzaTests.scala
@@ -0,0 +1,27 @@
+package com.ebaykorea.kyekang
+
+import junit.framework.TestCase
+import org.junit.Test
+import org.junit.Assert._
+/**
+  * Created by kyekang on 2016-11-08.
+  */
+class PizzaTests extends TestCase{
+
+  var pizza: Pizza = _
+
+  override def setUp = {
+    pizza = new Pizza
+  }
+
+  def testOneTopping = {
+    pizza.addTopping(Topping("green olives"))
+    assertEquals(pizza.getToppings.size, 1)
+  }
+
+  def testAddingAndRemovingToppings = {
+    pizza.addTopping(Topping("green olives"))
+    pizza.removeTopping(Topping("green olives"))
+    assertEquals(pizza.getToppings.size, 0)
+  }
+}
