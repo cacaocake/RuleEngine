ebd7e770871c92224bdccdd129cc689f9c1d383c
kyekang <kyekang@ebay.com>
2016-11-09 17:33:21 +0900

    구조 잡기... 생각하며 대충 넣은거라 내부에 선언된건 아무 의미 없음

--- src/main/scala/com/ebaykorea/kyekang/core/Condition.scala
+++ src/main/scala/com/ebaykorea/kyekang/core/Condition.scala
@@ -2,7 +2,8 @@ package com.ebaykorea.kyekang.core
 
 /**
   * Created by kyekang on 2016-11-09.
+  * req랑 profile은 아마 string이 key인 hashMap의 ref
   */
-class Condition(left: Operand, op: Operator, right: Operand) {
+class Condition(key: Long, req: String, profile: String) {
 
 }
